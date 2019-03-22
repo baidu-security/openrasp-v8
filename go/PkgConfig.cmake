7c04c1e52176bc3b59f3a45290a4f29726292cfb
lanyuhang <lanyuhang@baidu.com>
2019-03-22 16:29:30 +0800

    feat(base): support v8 with libc++ on linux

--- go/PkgConfig.cmake
+++ go/PkgConfig.cmake
@@ -1,4 +1,5 @@
 cmake_minimum_required(VERSION 3.0.0)
 set(PREFIX ${CMAKE_CURRENT_LIST_DIR})
+list(JOIN V8_LINK_LIBS " " V8_LINK_LIBS)
 configure_file(${PREFIX}/openrasp-v8.pc.in ${PREFIX}/openrasp-v8.pc)
 configure_file(${PREFIX}/v8.pc.in ${PREFIX}/v8.pc)
