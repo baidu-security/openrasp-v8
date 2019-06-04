7fb42fd62d4370794cc222d9d8ee1feed11d0384
lanyuhang <lanyuhang@baidu.com>
2019-06-04 16:39:59 +0800

    fix(base): flex sql utf8 syntax index

--- base/flex/flex.h
+++ base/flex/flex.h
@@ -1,8 +1,9 @@
+#include <inttypes.h>
 typedef struct flex_token_result {
-    int32_t * result;
-    int32_t result_len;
+    uint32_t * result;
+    uint32_t result_len;
 } flex_token_result;
 
-flex_token_result flex_lexing(const char *input, int32_t len, const char *tokenizer_type);
+flex_token_result flex_lexing(const char *input, uint32_t len, const char *tokenizer_type);
 
 #define YY_FATAL_ERROR(msg) throw msg
