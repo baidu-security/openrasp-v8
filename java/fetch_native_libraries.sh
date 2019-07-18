f5d30336865c92190a7627ae344de64a8f879d1c
lanyuhang <lanyuhang@baidu.com>
2019-07-18 11:41:28 +0800

    refactor(java): use curl to fetch native libraries

--- java/fetch_native_libraries.sh
+++ java/fetch_native_libraries.sh
@@ -5,12 +5,13 @@ ROOT=$(git rev-parse --show-toplevel)
 TAG=$(git describe --tags --abbrev=0)
 if [ $TRAVIS ]; then
   DIR=$HOME/cache
+  mkdir -p $DIR
 else
   DIR=/tmp
 fi
-wget -N -P $DIR https://github.com/baidu-security/openrasp-v8/releases/download/$TAG/java_natives_linux.tar.gz
-wget -N -P $DIR https://github.com/baidu-security/openrasp-v8/releases/download/$TAG/java_natives_osx.tar.gz
-wget -N -P $DIR https://github.com/baidu-security/openrasp-v8/releases/download/$TAG/java_natives_windows.tar.gz
+curl -s -L -o $DIR/java_natives_linux.tar.gz -z $DIR/java_natives_linux.tar.gz https://github.com/baidu-security/openrasp-v8/releases/download/$TAG/java_natives_linux.tar.gz
+curl -s -L -o $DIR/java_natives_osx.tar.gz -z $DIR/java_natives_osx.tar.gz https://github.com/baidu-security/openrasp-v8/releases/download/$TAG/java_natives_osx.tar.gz
+curl -s -L -o $DIR/java_natives_windows.tar.gz -z $DIR/java_natives_windows.tar.gz https://github.com/baidu-security/openrasp-v8/releases/download/$TAG/java_natives_windows.tar.gz
 tar zxf $DIR/java_natives_linux.tar.gz -C $ROOT/java/src/main/resources
 tar zxf $DIR/java_natives_osx.tar.gz -C $ROOT/java/src/main/resources
 tar zxf $DIR/java_natives_windows.tar.gz -C $ROOT/java/src/main/resources
