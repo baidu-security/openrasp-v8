1455c55a0c642fd882aebb9891ff1cdc85808fd1
lanyuhang <lanyuhang@baidu.com>
2019-07-18 11:40:34 +0800

    Revert "chore(java): add github auth header to fetch native libraries"
    This reverts commit 5f286d0e93e703c811af6e97a6f6bae4e01d1ca5.

--- java/fetch_native_libraries.sh
+++ java/fetch_native_libraries.sh
@@ -8,9 +8,9 @@ if [ $TRAVIS ]; then
 else
   DIR=/tmp
 fi
-wget --header "$GITHUB_AUTH_HEADER" -N -P $DIR https://github.com/baidu-security/openrasp-v8/releases/download/$TAG/java_natives_linux.tar.gz
-wget --header "$GITHUB_AUTH_HEADER" -N -P $DIR https://github.com/baidu-security/openrasp-v8/releases/download/$TAG/java_natives_osx.tar.gz
-wget --header "$GITHUB_AUTH_HEADER" -N -P $DIR https://github.com/baidu-security/openrasp-v8/releases/download/$TAG/java_natives_windows.tar.gz
+wget -N -P $DIR https://github.com/baidu-security/openrasp-v8/releases/download/$TAG/java_natives_linux.tar.gz
+wget -N -P $DIR https://github.com/baidu-security/openrasp-v8/releases/download/$TAG/java_natives_osx.tar.gz
+wget -N -P $DIR https://github.com/baidu-security/openrasp-v8/releases/download/$TAG/java_natives_windows.tar.gz
 tar zxf $DIR/java_natives_linux.tar.gz -C $ROOT/java/src/main/resources
 tar zxf $DIR/java_natives_osx.tar.gz -C $ROOT/java/src/main/resources
 tar zxf $DIR/java_natives_windows.tar.gz -C $ROOT/java/src/main/resources
