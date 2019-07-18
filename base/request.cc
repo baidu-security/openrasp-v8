80b034f351ddfd7b81a90ccd80ae61a1ee3f1872
lanyuhang <lanyuhang@baidu.com>
2019-07-18 20:31:52 +0800

    refactor(base): request method

--- base/request.cc
+++ base/request.cc
@@ -21,85 +21,94 @@ void request_callback(const v8::FunctionCallbackInfo<v8::Value>& info) {
     resolver->Reject(context, try_catch.Exception()).FromJust();
     return;
   }
+  auto undefined = v8::Undefined(isolate).As<v8::Value>();
   cpr::Session session;
   cpr::Header header;
   std::string method;
   {
     v8::HandleScope handle_scope(isolate);
-    auto tmp = config->Get(context, NewV8String(isolate, "method")).FromMaybe(v8::Local<v8::Value>());
-    if (!tmp.IsEmpty() && tmp->IsString()) {
+    auto tmp = config->Get(context, NewV8String(isolate, "method")).FromMaybe(undefined);
+    if (tmp->IsString()) {
       method = *v8::String::Utf8Value(isolate, tmp);
       std::transform(method.begin(), method.end(), method.begin(), ::tolower);
     }
   }
   {
     v8::HandleScope handle_scope(isolate);
-    auto tmp = config->Get(context, NewV8String(isolate, "url")).FromMaybe(v8::Local<v8::Value>());
-    if (!tmp.IsEmpty() && tmp->IsString()) {
+    auto tmp = config->Get(context, NewV8String(isolate, "url")).FromMaybe(undefined);
+    if (tmp->IsString()) {
       session.SetUrl(*v8::String::Utf8Value(isolate, tmp));
     }
   }
   {
     v8::HandleScope handle_scope(isolate);
-    auto tmp = config->Get(context, NewV8String(isolate, "params")).FromMaybe(v8::Local<v8::Value>());
-    if (!tmp.IsEmpty() && tmp->IsObject()) {
+    auto tmp = config->Get(context, NewV8String(isolate, "params")).FromMaybe(undefined);
+    if (tmp->IsObject()) {
       auto object = tmp.As<v8::Object>();
-      v8::Local<v8::Array> props = object->GetOwnPropertyNames(context).ToLocalChecked();
-      cpr::Parameters parameters;
-      for (int i = 0; i < props->Length(); i++) {
-        auto key = props->Get(context, i).ToLocalChecked();
-        auto val = object->Get(context, key).ToLocalChecked();
-        parameters.AddParameter({*v8::String::Utf8Value(isolate, key), *v8::String::Utf8Value(isolate, val)});
-      }
-      session.SetParameters(parameters);
-    }
-  }
-  {
-    v8::HandleScope handle_scope(isolate);
-    auto tmp = config->Get(context, NewV8String(isolate, "data")).FromMaybe(v8::Local<v8::Value>());
-    if (!tmp.IsEmpty()) {
-      if (tmp->IsObject()) {
-        auto json = v8::JSON::Stringify(context, tmp).FromMaybe(v8::String::Empty(isolate));
-        session.SetBody(cpr::Body(*v8::String::Utf8Value(isolate, json)));
-        header.emplace("content-type", "application/json");
-      } else if (tmp->IsString()) {
-        session.SetBody(cpr::Body(*v8::String::Utf8Value(isolate, tmp)));
-      } else if (tmp->IsArrayBuffer()) {
-        auto arraybuffer = tmp.As<v8::ArrayBuffer>();
-        auto content = arraybuffer->GetContents();
-        session.SetBody(cpr::Body(static_cast<const char*>(content.Data()), content.ByteLength()));
-        header.emplace("content-type", "application/octet-stream");
+      v8::Local<v8::Array> props;
+      if (object->GetOwnPropertyNames(context).ToLocal(&props)) {
+        cpr::Parameters parameters;
+        for (int i = 0; i < props->Length(); i++) {
+          v8::Local<v8::Value> key, val;
+          if (props->Get(context, i).ToLocal(&key) && object->Get(context, key).ToLocal(&val)) {
+            parameters.AddParameter({*v8::String::Utf8Value(isolate, key), *v8::String::Utf8Value(isolate, val)});
+          }
+        }
+        session.SetParameters(parameters);
       }
     }
   }
   {
     v8::HandleScope handle_scope(isolate);
-    auto tmp = config->Get(context, NewV8String(isolate, "maxRedirects")).FromMaybe(v8::Local<v8::Value>());
-    if (!tmp.IsEmpty() && tmp->IsInt32()) {
+    auto tmp = config->Get(context, NewV8String(isolate, "data")).FromMaybe(undefined);
+    if (tmp->IsObject()) {
+      v8::Local<v8::Value> json;
+      if (!v8::JSON::Stringify(context, tmp).ToLocal(&json)) {
+        resolver->Reject(context, try_catch.Exception()).FromJust();
+        return;
+      }
+      session.SetBody(cpr::Body(*v8::String::Utf8Value(isolate, json)));
+      header.emplace("content-type", "application/json");
+    } else if (tmp->IsString()) {
+      session.SetBody(cpr::Body(*v8::String::Utf8Value(isolate, tmp)));
+    } else if (tmp->IsArrayBuffer()) {
+      auto arraybuffer = tmp.As<v8::ArrayBuffer>();
+      auto content = arraybuffer->GetContents();
+      session.SetBody(cpr::Body(static_cast<const char*>(content.Data()), content.ByteLength()));
+      header.emplace("content-type", "application/octet-stream");
+    }
+  }
+  {
+    v8::HandleScope handle_scope(isolate);
+    auto tmp = config->Get(context, NewV8String(isolate, "maxRedirects")).FromMaybe(undefined);
+    if (tmp->IsInt32()) {
       session.SetMaxRedirects(cpr::MaxRedirects(tmp->Int32Value(context).FromJust()));
     }
   }
   {
     v8::HandleScope handle_scope(isolate);
-    auto tmp = config->Get(context, NewV8String(isolate, "timeout")).FromMaybe(v8::Local<v8::Value>());
-    if (!tmp.IsEmpty() && tmp->IsInt32()) {
+    auto tmp = config->Get(context, NewV8String(isolate, "timeout")).FromMaybe(undefined);
+    if (tmp->IsInt32()) {
       session.SetTimeout(tmp->Int32Value(context).FromJust());
     }
   }
   {
     v8::HandleScope handle_scope(isolate);
-    auto tmp = config->Get(context, NewV8String(isolate, "headers")).FromMaybe(v8::Local<v8::Value>());
-    if (!tmp.IsEmpty() && tmp->IsObject()) {
+    auto tmp = config->Get(context, NewV8String(isolate, "headers")).FromMaybe(undefined);
+    if (tmp->IsObject()) {
       auto object = tmp.As<v8::Object>();
-      v8::Local<v8::Array> props = object->GetOwnPropertyNames(context).ToLocalChecked();
-      for (int i = 0; i < props->Length(); i++) {
-        auto key = props->Get(context, i).ToLocalChecked();
-        auto val = object->Get(context, key).ToLocalChecked();
-        header.emplace(*v8::String::Utf8Value(isolate, key), *v8::String::Utf8Value(isolate, val));
+      v8::Local<v8::Array> props;
+      if (object->GetOwnPropertyNames(context).ToLocal(&props)) {
+        for (int i = 0; i < props->Length(); i++) {
+          v8::Local<v8::Value> key, val;
+          if (props->Get(context, i).ToLocal(&key) && object->Get(context, key).ToLocal(&val)) {
+            header.emplace(*v8::String::Utf8Value(isolate, key), *v8::String::Utf8Value(isolate, val));
+          }
+        }
       }
-      session.SetHeader(header);
     }
   }
+  session.SetHeader(header);
   session.SetVerifySsl(false);
   cpr::Response r;
   if (method == "get") {
@@ -120,35 +129,26 @@ void request_callback(const v8::FunctionCallbackInfo<v8::Value>& info) {
     r = session.Get();
   }
 
+  auto ret_val = v8::Object::New(isolate);
+  ret_val->Set(context, NewV8String(isolate, "config"), config).FromJust();
   if (r.error.code == cpr::ErrorCode::OK) {
-    auto ret_val = v8::Object::New(isolate);
-    ret_val->Set(context, NewV8String(isolate, "status"), v8::Int32::New(isolate, r.status_code)).FromJust();
-    auto data = v8::String::NewFromUtf8(isolate, r.text.data(), v8::NewStringType::kNormal, r.text.size())
-                    .FromMaybe(v8::String::Empty(isolate));
-    ret_val->Set(context, NewV8String(isolate, "data"), data).FromJust();
     auto headers = v8::Object::New(isolate);
     for (auto& h : r.header) {
-      auto key = v8::String::NewFromUtf8(isolate, h.first.data(), v8::NewStringType::kNormal, h.first.size());
-      auto val = v8::String::NewFromUtf8(isolate, h.second.data(), v8::NewStringType::kNormal, h.second.size());
-      if (key.IsEmpty() || val.IsEmpty()) {
-        continue;
-      }
-      headers->Set(context, key.ToLocalChecked(), val.ToLocalChecked()).FromJust();
+      auto key = NewV8String(isolate, h.first.data(), h.first.size());
+      auto val = NewV8String(isolate, h.second.data(), h.second.size());
+      headers->Set(context, key, val).FromJust();
     }
+    ret_val->Set(context, NewV8String(isolate, "status"), v8::Int32::New(isolate, r.status_code)).FromJust();
+    ret_val->Set(context, NewV8String(isolate, "data"), NewV8String(isolate, r.text.data(), r.text.size())).FromJust();
     ret_val->Set(context, NewV8String(isolate, "headers"), headers).FromJust();
-    ret_val->Set(context, NewV8String(isolate, "config"), config).FromJust();
     resolver->Resolve(context, ret_val).FromJust();
   } else {
-    auto ret_val = v8::Object::New(isolate);
     auto error = v8::Object::New(isolate);
-    error->Set(context, NewV8String(isolate, "code"), v8::Int32::New(isolate, static_cast<int32_t>(r.error.code)))
-        .FromJust();
-    auto message =
-        v8::String::NewFromUtf8(isolate, r.error.message.data(), v8::NewStringType::kNormal, r.error.message.size())
-            .FromMaybe(v8::String::Empty(isolate));
+    auto code = v8::Int32::New(isolate, static_cast<int32_t>(r.error.code));
+    auto message = NewV8String(isolate, r.error.message.data(), r.error.message.size());
+    error->Set(context, NewV8String(isolate, "code"), code).FromJust();
     error->Set(context, NewV8String(isolate, "message"), message).FromJust();
     ret_val->Set(context, NewV8String(isolate, "error"), error).FromJust();
-    ret_val->Set(context, NewV8String(isolate, "config"), config).FromJust();
     resolver->Reject(context, ret_val).FromJust();
   }
 }
