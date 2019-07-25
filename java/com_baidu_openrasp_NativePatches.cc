725698c79e132518b3875aca5ca1f257416b48f5
lanyuhang <lanyuhang@baidu.com>
2019-07-25 15:09:49 +0800

    refactor(java): update native patches

--- java/com_baidu_openrasp_NativePatches.cc
+++ java/com_baidu_openrasp_NativePatches.cc
@@ -18,15 +18,16 @@
 #ifdef __linux__
 #include <arpa/inet.h>
 #include <ifaddrs.h>
-#include <net/if.h>
+#include <linux/if_link.h>
+#include <net/ethernet.h>
 #include <netdb.h>
-#include <netinet/in.h>
-#include <sys/ioctl.h>
+#include <netpacket/packet.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
-#include <sys/sysctl.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
 #endif
@@ -44,61 +45,65 @@ JNIEXPORT jobjectArray JNICALL Java_com_baidu_openrasp_NativePatches_GetNetworkI
     env->DeleteLocalRef(ref);
   }
 #ifdef __linux__
-  struct ifconf ifc;
-  struct ifreq* ifreqP;
-  char* buf = nullptr;
-  int sock = socket(AF_INET, SOCK_DGRAM, 0);
-  if (sock < 0) {
-    return nullptr;
-  }
-  // do a dummy SIOCGIFCONF to determine the buffer size
-  // SIOCGIFCOUNT doesn't work
-  ifc.ifc_buf = nullptr;
-  if (ioctl(sock, SIOCGIFCONF, (char*)&ifc) < 0) {
-    return nullptr;
+  struct ifaddrs *ifaddr, *ifa;
+  char host[NI_MAXHOST] = {0};
+  char macp[INET6_ADDRSTRLEN] = {0};
+  std::map<std::string, struct ifaddrs*> inets;
+  std::map<std::string, struct ifaddrs*> inet6s;
+  std::map<std::string, struct ifaddrs*> packets;
+  std::vector<jstring> rst;
+
+  if (getifaddrs(&ifaddr) == -1) {
+    perror("getifaddrs");
+    return NULL;
   }
 
-  // call SIOCGIFCONF to enumerate the interfaces
-  if ((buf = (char*)malloc(ifc.ifc_len)) == nullptr) {
-    return nullptr;
-  }
-  ifc.ifc_buf = buf;
-  if (ioctl(sock, SIOCGIFCONF, (char*)&ifc) < 0) {
-    free(buf);
-    return nullptr;
-  }
-
-  std::vector<std::string> list;
-  // iterate through each interface
-  ifreqP = ifc.ifc_req;
-  for (int i = 0; i < ifc.ifc_len / sizeof(struct ifreq); i++, ifreqP++) {
-    // ignore non IPv4 addresses
-    if (ifreqP->ifr_addr.sa_family != AF_INET) {
+  for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+    if (ifa->ifa_addr == NULL) {
       continue;
     }
-    ioctl(sock, SIOCGIFADDR, ifreqP);
-    char* ip = inet_ntoa(((struct sockaddr_in*)&(ifreqP->ifr_addr))->sin_addr);
-    if (strncmp(ip, "127.0.0.1", sizeof("127.0.0.1") - 1) == 0) {
+    if (ifa->ifa_addr->sa_family == AF_PACKET) {
+      packets.emplace(ifa->ifa_name, ifa);
+    } else if (ifa->ifa_addr->sa_family == AF_INET) {
+      inets.emplace(ifa->ifa_name, ifa);
+    } else if (ifa->ifa_addr->sa_family == AF_INET6) {
+      inet6s.emplace(ifa->ifa_name, ifa);
+    }
+  }
+
+  for (auto it_packet : packets) {
+    auto it_inet = inets.find(it_packet.first);
+    auto it_inet6 = inet6s.find(it_packet.first);
+    if (it_inet == inets.end() && it_inet6 == inet6s.end()) {
       continue;
     }
-    list.emplace_back(ifreqP->ifr_name);
-    list.emplace_back(ip);
-    ioctl(sock, SIOCGIFHWADDR, ifreqP);
-    char tmp[30] = {0};
-    unsigned char* ptr = (unsigned char*)ifreqP->ifr_addr.sa_data;
-    sprintf(tmp, "%02x-%02x-%02x-%02x-%02x-%02x", *ptr, *(ptr + 1), *(ptr + 2), *(ptr + 3), *(ptr + 4), *(ptr + 5));
-    list.emplace_back(tmp);
+    rst.push_back(env->NewStringUTF(it_packet.first.c_str()));
+    struct sockaddr_ll* s = (struct sockaddr_ll*)(it_packet.second->ifa_addr);
+    int len = 0;
+    for (int i = 0; i < 6; i++) {
+      len += sprintf(macp + len, "%02x%s", (unsigned char)s->sll_addr[i], i < 5 ? "-" : "");
+    }
+    rst.push_back(env->NewStringUTF(macp));
+    if (it_inet == inets.end() || getnameinfo(it_inet->second->ifa_addr, sizeof(struct sockaddr_in), host, NI_MAXHOST,
+                                              NULL, 0, NI_NUMERICHOST) != 0) {
+      rst.push_back(NULL);
+    } else {
+      rst.push_back(env->NewStringUTF(host));
+    }
+    if (it_inet6 == inet6s.end() || getnameinfo(it_inet6->second->ifa_addr, sizeof(struct sockaddr_in6), host,
+                                                NI_MAXHOST, NULL, 0, NI_NUMERICHOST) != 0) {
+      rst.push_back(NULL);
+    } else {
+      rst.push_back(env->NewStringUTF(host));
+    }
   }
+  freeifaddrs(ifaddr);
 
-  jobjectArray rst = env->NewObjectArray(list.size(), jstrcls, nullptr);
-  for (int i = 0; i < list.size(); i++) {
-    env->SetObjectArrayElement(rst, i, env->NewStringUTF(list[i].c_str()));
+  jobjectArray jarr = env->NewObjectArray(rst.size(), jstrcls, nullptr);
+  for (int i = 0; i < rst.size(); i++) {
+    env->SetObjectArrayElement(jarr, i, rst[i]);
   }
-
-  // free buffer
-  free(buf);
-  close(sock);
-  return rst;
+  return jarr;
 #else
   return env->NewObjectArray(0, jstrcls, nullptr);
 #endif
