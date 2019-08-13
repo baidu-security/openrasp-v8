c6e199ebc162adc17e9aab6ed6a6c4d08066234d
lanyuhang <lanyuhang@baidu.com>
2019-08-13 20:42:03 +0800

    refactor(java): update fetch native libraries

--- java/fetch_native_libraries.sh
+++ java/fetch_native_libraries.sh
@@ -10,12 +10,9 @@ else
   DIR=/tmp
 fi
 echo $ROOT $TAG $DIR
-curl -L -o $DIR/java_natives_linux.tar.gz.download -z $DIR/java_natives_linux.tar.gz https://github.com/baidu-security/openrasp-v8/releases/download/$TAG/java_natives_linux.tar.gz
-mv $DIR/java_natives_linux.tar.gz.download $DIR/java_natives_linux.tar.gz || true
-tar zxf $DIR/java_natives_linux.tar.gz -C $ROOT/java/src/main/resources
-curl -L -o $DIR/java_natives_osx.tar.gz.download -z $DIR/java_natives_osx.tar.gz https://github.com/baidu-security/openrasp-v8/releases/download/$TAG/java_natives_osx.tar.gz
-mv $DIR/java_natives_osx.tar.gz.download $DIR/java_natives_osx.tar.gz || true
-tar zxf $DIR/java_natives_osx.tar.gz -C $ROOT/java/src/main/resources
-curl -L -o $DIR/java_natives_windows.tar.gz.download -z $DIR/java_natives_windows.tar.gz https://github.com/baidu-security/openrasp-v8/releases/download/$TAG/java_natives_windows.tar.gz
-mv $DIR/java_natives_windows.tar.gz.download $DIR/java_natives_windows.tar.gz || true
-tar zxf $DIR/java_natives_windows.tar.gz -C $ROOT/java/src/main/resources
+for OS in linux osx windows
+do
+  curl -L -o $DIR/java_natives_$OS.tar.gz.download -z $DIR/java_natives_$OS.tar.gz https://github.com/baidu-security/openrasp-v8/releases/download/$TAG/java_natives_$OS.tar.gz
+  [[ -f $DIR/java_natives_$OS.tar.gz.download ]] && mv $DIR/java_natives_$OS.tar.gz.download $DIR/java_natives_$OS.tar.gz
+  tar zxf $DIR/java_natives_$OS.tar.gz -C $ROOT/java/src/main/resources
+done
