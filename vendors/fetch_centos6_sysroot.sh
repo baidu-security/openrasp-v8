3b00fd2bef69212cbd2a135453a9ad2679e5c57a
lanyuhang <lanyuhang@baidu.com>
2020-03-26 18:16:20 +0800

    chore(base): update curl script

--- vendors/fetch_centos6_sysroot.sh
+++ vendors/fetch_centos6_sysroot.sh
@@ -9,6 +9,6 @@ else
   DIR=/tmp
 fi
 
-curl -L -o $DIR/centos6-sysroot.tar.gz.download -z $DIR/centos6-sysroot.tar.gz https://packages.baidu.com/app/openrasp/v8/centos6-sysroot.tar.gz
+curl -# -k -L -o $DIR/centos6-sysroot.tar.gz.download -z $DIR/centos6-sysroot.tar.gz https://packages.baidu.com/app/openrasp/v8/centos6-sysroot.tar.gz
 [[ -f $DIR/centos6-sysroot.tar.gz.download ]] && mv $DIR/centos6-sysroot.tar.gz.download $DIR/centos6-sysroot.tar.gz
 tar zxf $DIR/centos6-sysroot.tar.gz -C /tmp/
